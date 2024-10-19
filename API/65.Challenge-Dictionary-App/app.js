55f943cd1e50ef8b7f04afa3ae2de14fc19be71d
ravi-sidharth <thenewravy@gmail.com>
2024-10-19 06:04:13 +0530

    update

--- API/65.Challenge-Dictionary-App/app.js
+++ API/65.Challenge-Dictionary-App/app.js
@@ -1,19 +1,60 @@
 const input = document.querySelector('input')
 const button = document.querySelector('button')
 
-const definition = document.getElementById('definition')
-const partsOfSpeech = document.getElementById('partsOfSpeech')
-const example = document.getElementById('example')
-const synonyms = document.getElementById('synonyms')
-const antonyms = document.getElementById('antonyms')
 
-button.addEventListener('click',()=> {
-    const word = input.value 
+
+button.addEventListener('click', () => {
+    const word = input.value
     fetchData(word)
 })
 
 async function fetchData(word) {
-    const res = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
-    const data = await res.json()
-    console.log(data)
+    try {
+        const res = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
+        const data = await res.json()
+        console.log(data)
+        showResult(data)
+    } catch (e) {
+        console.log("ERROR: ", e)
+
+    }
+
+
 }
+
+
+
+function showResult(data) {
+    let wordMeaning = document.querySelector('#word-meaning')
+    let templateWordMeaning = document.querySelector('#template-word-meaning')
+
+    wordMeaning.innerHTML=""
+    const cloneWord = templateWordMeaning.content.cloneNode(true)
+
+    const definition = cloneWord.getElementById('definition')
+    const partsOfSpeech = cloneWord.getElementById('partsOfSpeech')
+    const example = cloneWord.getElementById('example')
+    const synonyms = cloneWord.getElementById('synonyms')
+    const antonyms = cloneWord.getElementById('antonyms')
+    definition.innerHTML =`Definition: ${data[0].meanings[0].definitions[0].definition}`
+    partsOfSpeech.innerText = `Part Of Speech: ${data[0].meanings[0].partOfSpeech}`
+    if (data[0]?.meanings[0]?.definitions[0]?.example) {
+         example.innerText = `Example: ${data[0].meanings[0].definitions[0].example}`
+    } else{
+        example.innerText="" 
+    } 
+    if (data[0]?.meanings[0]?.antonyms && data[0]?.meanings[0]?.antonyms.length>0) {
+        synonyms.innerText = `Synonyms: ${data[0].meanings[0].antonyms}`
+    } else {
+        synonyms.innerText=""
+    }
+
+    if (data[0].meanings[0].synonyms && data[0].meanings[0].synonyms.length>0) {
+        antonyms.innerText = `Antonyms: ${data[0].meanings[0].synonyms}`
+    } else{
+        antonyms.innerText=""
+    }
+
+    wordMeaning.appendChild(cloneWord)
+}
+
