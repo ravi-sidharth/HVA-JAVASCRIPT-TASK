6b054fdcc6d3157fa0ace94c2312bdb5e234554c
ravi-sidharth <thenewravy@gmail.com>
2024-10-22 12:05:13 +0530

    update

--- API/64.Fetch and Display Posts and Comments by User ID/user-posts-comments.js
+++ API/64.Fetch and Display Posts and Comments by User ID/user-posts-comments.js
@@ -2,15 +2,15 @@ const button = document.querySelector('button')
 const input = document.querySelector('input')
 const post = document.getElementById('posts')
 
-button.addEventListener('click',()=> {
-    const userId =input.value
+button.addEventListener('click', () => {
+    const userId = input.value
     console.log(userId)
     fetchData(userId)
 
 })
 
 async function fetchData(userId) {
-    const res1 =await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${userId}`)
-    const data =await res1.json()
+    const res1 = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${userId}`)
+    const data = await res1.json()
     console.log(data)
 }
