13e241005a55feded7d0f572111ef52e25a239a7
ravi-sidharth <thenewravy@gmail.com>
2024-10-23 18:29:23 +0530

    update

--- API/64.Fetch and Display Posts and Comments by User ID/user-posts-comments.js
+++ API/64.Fetch and Display Posts and Comments by User ID/user-posts-comments.js
@@ -6,11 +6,21 @@ button.addEventListener('click', () => {
     const userId = input.value
     console.log(userId)
     fetchData(userId)
-
 })
 
 async function fetchData(userId) {
-    const res1 = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${userId}`)
-    const data = await res1.json()
+    const res = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${userId}`)
+    const data = await res.json()
     console.log(data)
+
+    fetchPosts(data)  
+}
+
+function fetchPosts(data) {
+    data.forEach(async(post)=>{
+        console.log(post.id)
+        const res1 = await fetch(`https://jsonplaceholder.typicode.com/comments?postId=${post.id}`)
+        const data1= await res1.json()
+        console.log(data1)
+    })
 }
--- Weather Dashboard/app.js
+++ Weather Dashboard/app.js
@@ -6,24 +6,33 @@ formSubmit.addEventListener('submit',(event)=> {
     event.preventDefault()
 
     cityName = input.value 
-    fetchWeather("Delhi")
+    fetchWeather(cityName)
+    
 })
 
-async function fetchWeather(city) {
-    const res = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=84c8a6982ed02cee6d19c6df55534ff2`)
+async function fetchWeather() {
+    const res = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=${apiKey}`)
     const data = await res.json()
     console.log(data)
-
-    let lat = data.lat
-    let lon = data.lon
-
-    const res1 = await fetch(`http://api.openweathermap.org/data/2.5/forecast?lat=28.6517178&lon=77.2219388&appid=84c8a6982ed02cee6d19c6df55534ff2`)
+    console.log("CityName",data[0].name)
+    let lat = data[0].lat
+    // console.log(lat)
+    let lon = data[0].lon
+    // console.log(lon);
+    
+    const res1 = await fetch(`http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`)
     const data1 = await res1.json()
     console.log(data1)      
-    bindData(res1)
+    bindData(data1)
 }
 
 function bindData(data1) {
+    console.log(data1)
+    console.log("Humidity",data1.list[0].main.humidity)
+    console.log("Temperature",data1.list[0].main.temp)
+    console.log("Description",data1.list[0].weather[0].description)
+    console.log("Wind Speed",data1.list[0].wind.speed)
+
     const weatherDetail =document.getElementById('weather-detail')
     const weatherTemplate =document.getElementById('weather-template')
 
@@ -34,5 +43,13 @@ function bindData(data1) {
     const description = weatherClone.getElementById('cityName')
     const humadityPercentage = weatherClone.getElementById('cityName')
     const windSpeed = weatherClone.getElementById('cityName')
+
+    date.innerText =""
+    cityName.innerText =""
+    temperature.innerText =""
+    description.innerText =""
+    humadityPercentage.innerText =""
+    windSpeed.innerText =""
+    
 
 }
