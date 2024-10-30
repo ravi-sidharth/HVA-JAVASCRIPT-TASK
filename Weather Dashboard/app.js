713d79a189a43aeac7b2236251c4fe62831c6f98
ravi-sidharth <thenewravy@gmail.com>
2024-10-30 09:45:18 +0530

    update

--- Weather Dashboard/app.js
+++ Weather Dashboard/app.js
@@ -5,78 +5,79 @@ let cityName =""
 let lat =""
 let lon=""
 
-
 const weatherDetail =document.getElementById('weather-detail')
 const weatherTemplate =document.getElementById('weather-template')
 
 formSubmit.addEventListener('submit',(event)=> {
     event.preventDefault()
-
     cityName = input.value 
     fetchWeather(cityName)
-    
 })
 
-async function fetchWeather() {
+async function fetchWeather(cityName) {
    try {
-     const res = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=${apiKey}`)
+    const res = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=${apiKey}`)
     const data = await res.json()
-    console.log("CityName",data[0].name)
-    
+    // console.log("CityName",data[0].name)
     lat = data[0].lat
     lon = data[0].lon
-    
-    // const res1 = await fetch(`http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`)
+
     const res1 = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`)
     const data1 = await res1.json()
-    console.log(data1)      
-    bindData(data1)
+    // console.log(data1)      
+    bindWeather(data1)
    } catch(e) {
-        weatherDetail.innerText=`Error Input city is not exits!`
+        alert(`Error Input city is not exits! ,change another city ${e}`)
    }
 }
 
-function bindData(data1) {
-    const TodayDate = new Date((data1.timezone)*1000).toLocaleString()
-    
+function bindWeather(data1) {
+    const TodayDate = new Date((data1.dt)*1000).toLocaleString()
+
     const weatherClone = weatherTemplate.content.cloneNode(true)
     const cityNameEle = weatherClone.getElementById('cityName')
     const date = weatherClone.getElementById('date')
     const temperature= weatherClone.getElementById('temperature')
     const description = weatherClone.getElementById('description')
-    const humadityPercentage = weatherClone.getElementById('humadityPercentage')
+    const humidityPercentage = weatherClone.getElementById('humidityPercentage')
     const windSpeed = weatherClone.getElementById('windSpeed')
+    const dataHeader = weatherClone.getElementById('data')
 
-    weatherDetail.innerText="This is Current Weather Data"
+    dataHeader.innerText="👇👇Today's weather data is below👇👇"
     date.innerText =`Date: ${TodayDate}`
     cityNameEle.innerText =`City Name: ${cityName}`
-    temperature.innerText =`Temperature: ${data1.main.temp} Kelvin`
+    temperature.innerText =`Temperature: ${data1.main.temp} Celcius`
     description.innerText =`Description: ${data1.weather[0].description}`
-    humadityPercentage.innerText =`Humidity: ${data1.main.humidity}%`
-    windSpeed.innerText =`Wind Speep: ${data1.wind.speed} meter/sec` 
+    humidityPercentage.innerText =`Humidity: ${data1.main.humidity}%`
+    windSpeed.innerText =`Wind Speed: ${data1.wind.speed} meter/sec` 
     weatherDetail.appendChild(weatherClone)
-    forecast()
+    forecastWeather()
 }
 
-async function forecast() {
-    console.log(lat,lon)
-    const res2 = await fetch(`https://api.openweathermap.org/data/2.5/forecast/daily?lat=${lat}&lon=${lon}&cnt=5&appid=${apiKey}`)
-    const data2 = await res2.json()
-    console.log(data2)
+async function forecastWeather() {
+    // console.log(lat,lon)
+    try {
+        const res2 = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`)
+        const data2 = await res2.json()
+        // console.log(data2)
 
-    const weatherDetail =document.getElementById('weather-detail')
-    const weatherTemplate =document.getElementById('weather-template')
-    data2.list.forEach(forecastItem=> {
-        const weatherClone = weatherTemplate.content.cloneNode(true)
-
-        const forcastDate = weatherClone.getElementById('date')
-        const temperature= weatherClone.getElementById('temperature')
-        const description = weatherClone.getElementById('description')
-        
-        forcastDate.innerHTML = new Date((forecastItem.dt)*1000).toLocaleString()
-        temperature.innerHTML = forecastItem.main.temp
-        description.innerHTML = forecastItem.weather[0].description
-        weatherDetail.appendChild(weatherClone)
-    })
-    
+        let count=1
+        for (let i=7; i<data2.list.length; i+=8) {
+            const weatherClone = weatherTemplate.content.cloneNode(true)
+            const dataHeader = weatherClone.getElementById('data')
+            const forcastDate = weatherClone.getElementById('date')
+            const temperature= weatherClone.getElementById('temperature')
+            const description = weatherClone.getElementById('description')
+            
+            dataHeader.innerText=`👇Day ${count} weather data is Below👇`
+            forcastDate.innerHTML = `Date: ${data2.list[i].dt_txt}`
+            temperature.innerHTML = `Expected Temperature: ${data2.list[i].main.temp} Celcius`
+            description.innerHTML = `Description: ${data2.list[i].weather[0].description} and Icon ${data2.list[i].weather[0].icon}`
+            weatherDetail.appendChild(weatherClone)
+            count++
+        }
+    } catch(e) {
+        weatherDetail.innerText=`Error API is not working! or ${e}`
+   }
+   
 }
